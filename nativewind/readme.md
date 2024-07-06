7c34b323594cefbb607a3ba027f1b9b52dc0e057
feng-sifang <sfang.dev@gmail.com>
2024-07-06 21:52:52 +0800

    Modified prettier

---
+++ nativewind/readme.md
@@ -0,0 +1,10 @@
+# Start w/ expo
+
+[Doc](https://www.nativewind.dev/quick-starts/expo)
+
+```bash
+npm install nativewind
+npm install --save-dev tailwindcss@3.3.2
+```
+
+Run `npx tailwindcss init` to create a tailwind.config.js file.
--- nativewind/tailwind.config.js
+++ nativewind/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}", 
+    "./app/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
